e8bcf310ab27b14d6cb9c947497131d86866ad6b
4ikkamony <compbanderloga@gmail.com>
2025-02-27 14:38:50 +0200

    Update settings.py
    Add cors app and middleware
    
    Add drf_spectacular

--- core/settings.py
+++ core/settings.py
@@ -34,12 +34,15 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "rest_framework",
+    "drf_spectacular",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
