8f99ab87ea7802e4f913fefab76c65bf5b03abc9
4ikkamony <compbanderloga@gmail.com>
2025-02-27 16:29:23 +0200

    Add apps urls to core urls

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,12 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
+    path("api/books/", include("book_service.urls", namespace="book_service")),
+    path(
+        "api/borrowings/",
+        include("borrowing_service.urls", namespace="borrowing_service"),
+    ),
+    path("api/payments/", include("payment_service.urls", namespace="payment_service")),
     path("api/users/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
@@ -11,5 +17,5 @@ urlpatterns = [
         name="swagger-ui",
     ),
     path("admin/", admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls"))
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
