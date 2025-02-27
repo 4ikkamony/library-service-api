e34c3f61f003a688694df3331071198f6b75796d
4ikkamony <compbanderloga@gmail.com>
2025-02-27 15:15:24 +0200

    Update core urls
    Add user app urls to urlpatterns
    
    Add swagger views to urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
+    path("api/users/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls"))
 ]
