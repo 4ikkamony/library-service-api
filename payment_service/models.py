822cbf9cfed3e42b2390ee0b30045f41cf0e799c
4ikkamony <compbanderloga@gmail.com>
2025-02-27 16:03:45 +0200

    Initialize payment_service app
    Add basic Payment model

---
+++ payment_service/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from borrowing_service.models import Borrowing
+
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "pending", "Pending"
+        PAID = "paid", "Paid"
+
+    class Type(models.TextChoices):
+        PAYMENT = "payment", "Payment"
+        FINE = "fine", "Fine"
+
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.CASCADE, related_name="payment"
+    )
+    session_url = models.URLField(max_length=400)
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(max_digits=6, decimal_places=2)
