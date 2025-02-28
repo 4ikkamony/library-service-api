38a0dfb073a2950a8f90eb9599f7699fc01c703e
Ihor Halytskiy <galytskiyihor@gmail.com>
2025-02-28 17:28:45 +0200

    create test

--- user/tests.py
+++ user/tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
@@ -20,6 +21,46 @@ class AccountsTests(TestCase):
             "password": "testpassword",
         }
         self.client.post(REGISTER_URL, self.user_data)
+
+    def test_create_user_without_username(self):
+        user_data = {
+            "first_name": "Test",
+            "last_name": "User",
+            "email": "unique@test.com",
+            "password": "testpassword",
+        }
+
+        response = self.client.post(REGISTER_URL, user_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertNotIn("username", response.data)
+        self.assertIn("email", response.data)
+        self.assertEqual(user_data["email"], response.data["email"])
+
+    def test_create_users_with_duplicate_email(self):
+        user_data = {
+            "first_name": "testfirst",
+            "last_name": "testlast",
+            "email": "test_1@test.com",
+            "password": "testpassword",
+        }
+        response1 = self.client.post(REGISTER_URL, user_data)
+
+        self.assertEqual(response1.status_code, status.HTTP_201_CREATED)
+
+        user_data_2 = {
+            "first_name": "testfirstduplicate",
+            "last_name": "testlastduplicate",
+            "email": "test_1@test.com",
+            "password": "testpassword",
+        }
+        response2 = self.client.post(REGISTER_URL, user_data_2)
+
+        self.assertEqual(response2.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response2.data["email"][0],
+            "user with this email address already exists.",
+        )
 
     def test_user_without_first_name(self):
         user_data = {
@@ -58,6 +99,8 @@ class AccountsTests(TestCase):
         self.assertIn("access", response.data)
         self.assertIn("refresh", response.data)
 
+
+
     def test_get_page_me(self):
         token_data = {
             "email": "testemail@test.com",
@@ -72,3 +115,6 @@ class AccountsTests(TestCase):
         )
 
         self.assertEqual(response2.status_code, status.HTTP_200_OK)
+
+    def tearDown(self):
+        get_user_model().objects.all().delete()
