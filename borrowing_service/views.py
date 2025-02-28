b3cf063190906a7145b4605685bd6e2bbfef9bca
Arsen Myroniuk <arisenmyr@gmail.com>
2025-02-28 19:55:22 +0200

    Add ViewSet for Borrowing: -Create, List, Retrieve

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from borrowing_service.models import Borrowing
+from borrowing_service.serializers import (
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+    BorrowingCreateSerializer,
+    BorrowingSerializer
+)
+
+
+class BorrowingViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet
+):
+    queryset = Borrowing.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        if self.action == "create":
+            return BorrowingCreateSerializer
+
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
