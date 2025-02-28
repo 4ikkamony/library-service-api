31cf799db666066e25bb2eb647b7fee0f0825e38
IvankaKuzin <kuziniwanka@gmail.com>
2025-02-28 18:31:13 +0200

    add view for 'payment' model, but without filtering user

--- payment_service/views.py
+++ payment_service/views.py
@@ -10,14 +10,6 @@ from payment_service.serializers import PaymentSerializer, PaymentListSerializer
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
 
-    def get_queryset(self):
-        queryset = self.queryset
-        user = self.request.data.get("borrowing_user")
-        if user["is_staff"]:
-            return queryset
-        else:
-            return queryset.filter(user=self.request.user)
-
     def get_serializer_class(self):
         if self.action == "list":
             return PaymentListSerializer
