6b6a5d29d31bd3226f59a71132aac8501809fb29
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-02-28 19:25:10 +0200

    Added serializers, added validation to model

---
+++ book_service/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from book_service.models import Book
+
+
+class BaseBookSerializer(serializers.ModelSerializer):
+    def validate_daily_fee(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Daily fee has to be greater than 0.")
+        return value
+
+class BookSerializer(BaseBookSerializer):
+    class Meta:
+        model = Book
+        fields = ["id", "title", "author", "inventory", "daily_fee"]
+
+class BookListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author")
+
+class BookDetailSerializer(BaseBookSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
