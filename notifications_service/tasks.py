cc5d82b5c2f7e2cb218e130b024c2783f16cd2f8
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-03-01 12:16:51 +0200

    fix: fixed F821 error & moved example to tasks

--- notifications_service/tasks.py
+++ notifications_service/tasks.py
@@ -1,12 +1,28 @@
+import logging
 from celery import shared_task
-from .utils import send_telegram_message
+
+from notifications_service.utils import send_telegram_message
+
+# Used for Celery logging via:
+# celery -A core.celery_config worker -l info
+# Can be used in core/settings.py if needed
+logger = logging.getLogger(__name__)
 
 
-# TODO: remove with notify new borrowing
-@shared_task
-def send_test_telegram_message():
-    """Test task for sending a message to Telegram."""
-    message = "Test notification!"
-    success = send_telegram_message(message)
-    if not success:
-        raise Exception("Failed to send test Telegram message")
+@shared_task(max_retries=3, bind=True)
+def notify_new_borrowing(self, borrowing_id, user_email, book_title) -> None:
+    try:
+        logger.info(f"Processing notify_new_borrowing for borrowing_id={borrowing_id}")
+        message = (
+            f"New Borrowing Created!\n"
+            f"Borrowing ID: {borrowing_id}\n"
+            f"User: {user_email}\n"
+            f"Book: {book_title}"
+        )
+        success = send_telegram_message(message)
+        if not success:
+            logger.error("Failed to send Telegram notification")
+            raise Exception("Failed to send Telegram notification")
+    except Exception as exc:
+        logger.error(f"Error in notify_new_borrowing: {str(exc)}")
+        raise self.retry(exc=exc, countdown=60)
