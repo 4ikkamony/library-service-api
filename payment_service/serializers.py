424699c5e511d4843caa90be517c40f73fb28978
IvankaKuzin <kuziniwanka@gmail.com>
2025-03-01 11:50:56 +0200

    initialization 'borrowing' field in Payment serializer

--- payment_service/serializers.py
+++ payment_service/serializers.py
@@ -1,23 +1,28 @@
 from rest_framework import serializers
 
 from payment_service.models import Payment
+from borrowing_service.serializers import BorrowingSerializer, BorrowingListSerializer
 
 
-class PaymentSerializer(serializers.Serializer):
+class PaymentSerializer(serializers.ModelSerializer):
+    borrowing = BorrowingSerializer(many=False, read_only=True)
+
     class Meta:
         model = Payment
         fields = (
             "id",
             "borrowing",
-            "session_url",
-            "session_id",
-            "money_to_pay",
             "status",
             "type",
+            "money_to_pay",
+            "session_url",
+            "session_id",
         )
 
 
-class PaymentListSerializer(serializers.Serializer):
+class PaymentListSerializer(serializers.ModelSerializer):
+    borrowing = BorrowingListSerializer(many=False, read_only=True)
+
     class Meta:
         model = Payment
         fields = (
