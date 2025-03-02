15e2580de4bc27937e2a7dc99378f9aad335ce23
IvankaKuzin <kuziniwanka@gmail.com>
2025-03-02 11:38:01 +0200

    add session_expires_at field on Payment model

--- payment_service/models.py
+++ payment_service/models.py
@@ -1,12 +1,20 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 from borrowing_service.models import Borrowing
+
+
+def datetime_from_timestamp(timestamp: int):
+    return datetime.datetime.fromtimestamp(timestamp, datetime.timezone.utc)
 
 
 class Payment(models.Model):
     class Status(models.TextChoices):
         PENDING = "pending", "Pending"
         PAID = "paid", "Paid"
+        EXPIRED = "expired", "Expired"
 
     class Type(models.TextChoices):
         PAYMENT = "payment", "Payment"
@@ -17,6 +25,7 @@ class Payment(models.Model):
     )
     session_url = models.URLField(max_length=400)
     session_id = models.CharField(max_length=255)
+    session_expires_at = models.DateTimeField(default=timezone.now)
     money_to_pay = models.DecimalField(max_digits=6, decimal_places=2)
     status = models.CharField(
         max_length=10, choices=Status.choices, default=Status.PENDING
