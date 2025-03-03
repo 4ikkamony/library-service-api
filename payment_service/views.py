0a15da6ca45eb5f929f78b52865e97381067afbd
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-03-03 09:23:53 +0200

    Changed 'is_superuser' to 'is_staff' at views

--- payment_service/views.py
+++ payment_service/views.py
@@ -19,7 +19,7 @@ class ListPaymentView(generics.ListAPIView):
 
     def get_queryset(self):
         queryset = self.queryset
-        if self.request.user.is_superuser:
+        if self.request.user.is_staff:
             return queryset
         else:
             return queryset.filter(borrowing__user=self.request.user.id)
@@ -31,7 +31,7 @@ class DetailPaymentView(generics.RetrieveAPIView):
 
     def get_queryset(self):
         queryset = self.queryset
-        if self.request.user.is_superuser:
+        if self.request.user.is_staff:
             return queryset
         else:
             return queryset.filter(borrowing__user=self.request.user.id)
