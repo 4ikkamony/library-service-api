a2ea4960ba17ab2a99d2e3e645538d5b9be20973
4ikkamony <compbanderloga@gmail.com>
2025-03-14 15:31:27 +0200

    Fix code style

--- core/urls.py
+++ core/urls.py
@@ -12,7 +12,10 @@ urlpatterns = [
                     "borrowings/",
                     include("borrowing_service.urls", namespace="borrowing_service"),
                 ),
-                path("payments/", include("payment_service.urls", namespace="payment_service")),
+                path(
+                    "payments/",
+                    include("payment_service.urls", namespace="payment_service"),
+                ),
                 path("users/", include("user.urls", namespace="user")),
                 path("schema/", SpectacularAPIView.as_view(), name="schema"),
                 path(
@@ -21,7 +24,7 @@ urlpatterns = [
                     name="swagger-ui",
                 ),
             ]
-        )
+        ),
     ),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
